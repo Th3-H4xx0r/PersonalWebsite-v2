aec684d30d8e578f78fe525defb419962784e941
Pranav Krishna <krishnatechpranav@gmail.com>
2020-05-31 14:08:49 -0700

    12

---
+++ js/script.js
@@ -0,0 +1,52 @@
+
+function getGithubContributions() {
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open("GET", "https://github-contributions-api.now.sh/v1/Th3-H4xx0r", true);
+    xmlHttp.send();
+
+
+
+
+    xmlHttp.onreadystatechange = (e) => {
+        var total = 0;
+
+            var response = xmlHttp.responseText;
+
+            var years = JSON.parse(response)["years"];
+
+            for (var i = 0; i <= years.length; i++) {
+                if (years[i] != undefined || years[i] != null) {
+                    total = total + years[i]["total"];
+                }
+
+            }
+
+            console.log("total:" + total.toString());
+            $("#commits").html(total);
+            
+
+    }
+
+}
+
+function getGitHubRepos(){
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open("GET", "https://api.github.com/users/Th3-H4xx0r/repos", true);
+    xmlHttp.send();
+
+    xmlHttp.onreadystatechange = (e) => {
+        var totalRepos = 0;
+
+            var response = xmlHttp.responseText;
+
+            var repos = JSON.parse(response);
+
+            totalRepos = repos.length;
+
+
+            console.log("total repos:" + totalRepos.toString());
+            $("#repos").html(totalRepos);
+            
+
+    }
+}
